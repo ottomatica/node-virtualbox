d9f49df4bd2db6e6a01b62cae41c9edaa31d6d94
Samim Mirhosseini <samim.mirhoseini@gmail.com>
2018-07-19 11:06:23 -0400

    updated package.json, starting vm in index.js

--- index.js
+++ index.js
@@ -12,7 +12,16 @@ module.exports = async function(options = {}) {
     //options.language = options.language || 'python';
     let provider = new VBoxProvider();
     await provider.provision(options.vmname, options.ovf, options.verbose);
-
+    await provider.customize(options.vmname, undefined, undefined, options.verbose);
+    await provider.start(options.vmname, options.verbose);
     //return Bluebird.fromCallback(cb => generator.generate(JSON.stringify(data), cb));
+
+    // basic index file can be something like:
+    // • check() to verify whether things like vmname already is running/or box exists
+    // • provision() to start vm
+    // • customize() to run modifyvm stuff
+    // • start() -- optionally start()
+    // • post-setup() things that will require ssh.exec (setup /etc/networking/interfaces)
+
 
 };
